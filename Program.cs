30ac796b38324a3890c8dad8cb8d6cd763655bf7
Jun Shimura <junshimura@gmail.com>
2021-10-07 17:20:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestforGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
